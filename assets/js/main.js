94b819d3496144248d3a43984862da17b9e995cd
Alina <alinaasylbek4@gmail.com>
2025-10-30 16:09:55 +0500

    feat: добавить интерактив для персонажей Italian Brainrot

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
-alert("Добро пожаловать на мой сайт!");
+document.querySelectorAll('.character').forEach(char => {
+  const desc = char.querySelector('.description');
+  const text = char.dataset.description;
+
+  char.addEventListener('click', () => {
+    if (desc.innerText === text) {
+      desc.innerText = '';
+    } else {
+      desc.innerText = text;
+    }
+  });
+});
